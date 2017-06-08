64540e5e647544326fb70018ec85cc4b607db0dc
Aditya Verma <adityaverma2010@hotmail.com>
2017-06-08 13:53:12 +0530

    remove files

--- application/views/frontend/include/listing-sidebar.php
+++
@@ -1,5 +0,0 @@
-<ul>
-    <li  class="active"><a href="<?php echo base_url()?>Listing/listing">Your Listings</a></li>
-    <li ><a href="<?php echo base_url()?>Listing/listing2">Your Reservations</a></li>
-    <li><a href="<?php echo base_url()?>Listing/listing3">Reservation Requirements</a></li>
-</ul>
