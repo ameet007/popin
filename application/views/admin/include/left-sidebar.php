3b281b9de1e1039c58d34831ceabc5ecd149a5a0
Vanak Aliasgar <vanak@neurons-it.in>
2017-06-10 11:01:40 +0530

    remove unwanted dashboard link

--- application/views/admin/include/left-sidebar.php
+++ application/views/admin/include/left-sidebar.php
@@ -7,19 +7,17 @@
                         	<li class="menu-title">Navigation</li>
 
                             <li class="has_sub">
-                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
-                                <ul class="list-unstyled">
-                                    <li><a href="<?php echo base_url('admin')?>Siteowner/dashboard">Dashboard</a></li>
-                                    <!--<li><a href="dashboard_2.html">Dashboard 2</a></li>-->
-                                </ul>
+                                <a href="<?= base_url('admin'); ?>" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Dashboard </span> </a>
                             </li>
                             <li class="has_sub">
                                 <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-invert-colors"></i> <span> Front </span> <span class="menu-arrow"></span></a>
                                 <ul class="list-unstyled">
                                     <li><a href="<?= base_url(ADMIN_DIR.'/banner/lists'); ?>"><i class="fa fa-image"></i>Home Banner</a></li>
                                     <li><a href="<?= base_url(ADMIN_DIR.'/pages/lists'); ?>"><i class="fa fa-clone"></i>CMS Page</a></li>
-                                     <li><a href="<?= base_url(ADMIN_DIR.'/faq/lists'); ?>"><i class="fa fa-clone"></i>CMS Page</a></li>
+                                    <li><a href="<?= base_url(ADMIN_DIR.'/faq_category/lists'); ?>"><i class="fa fa-info"></i>FAQ Category</a></li>
+                                    <li><a href="<?= base_url(ADMIN_DIR.'/faq/lists'); ?>"><i class="fa fa-question"></i>FAQ</a></li>
 									<li><a href="<?= base_url(ADMIN_DIR.'/contact_request/lists'); ?>"><i class="fa fa-envelope"></i>Contact Req.</a></li>
+									<li><a href="<?= base_url(ADMIN_DIR.'/email_template/lists'); ?>"><i class="fa fa-envelope-o"></i>Email Templates</a></li>
                                 </ul>
                             </li>
                             <li>
