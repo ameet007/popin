a65da8c4c6e7a20dfa6f2e28458f2f49f4947481
rajesh <rajesh@neurous-it.in>
2017-07-13 17:30:57 +0530

    testing

--- application/views/frontend/dashboard.php
+++ application/views/frontend/dashboard.php
@@ -30,7 +30,7 @@
                             <img src="<?= base_url('uploads/user/thumb/'.$avatar); ?>" alt="">
                         </div>
                         <div class="pro-content">
-                            <h3><?= $userProfileInfo->firstName; ?></h3>
+                            <h3><?= ucfirst($userProfileInfo->firstName); ?></h3>
                             <a href="<?= base_url('user/profile'); ?>">Edit Profile</a>
                         </div>
                     </div>
@@ -75,7 +75,7 @@
                             <div class="panel-heading">Notifications</div>
                             <div class="panel-body">
                                 <ul>
-                                    <li class="clearfix"><a href="#"><?= $userProfileInfo->firstName; ?>, new spaces have arrived! Book now before they run out.</a><span class="pull-right"><a href="#"><img src="<?= base_url('theme/front/assests/img/close-icon.png'); ?>" alt=""></a></span></li>
+                                    <li class="clearfix"><a href="#">Cassidy, new spaces have arrived! Book now before they run out.</a><span class="pull-right"><a href="#"><img src="<?= base_url('theme/front/assests/img/close-icon.png'); ?>" alt=""></a></span></li>
                                     <li class="clearfix"><a href="#">Book workshops. led by experienced business owners. Now over, 51 to choose form.</a><span class="pull-right"><a href="#"><img src="<?= base_url('theme/front/assests/img/close-icon.png'); ?>" alt=""></a></span></li>
                                     <li class="clearfix"><a href="#">Invite your friend to join Popln and you’ll get $10 after their first rental.</a><span class="pull-right"><a href="#"><img src="<?= base_url('theme/front/assests/img/close-icon.png'); ?>" alt=""></a></span></li>
                                 </ul>
@@ -112,7 +112,96 @@
                                 <?php endif;?>
                             </div>
                         </div>
-                        
+                        <!-- Display upload docuemnt file -->            
+                       <div class="panel panel-default profile-photo verified-info">
+                            <div class="panel-heading">Documents</div>
+                            <div class="panel-body">
+                                <div class="row mr15">
+                                    <div class="col-md-9">
+                                            <?php 
+                                            if($userProfileInfo->licenceCopy!='')
+                                            {
+                                                $licenceLink = base_url('uploads/user/document/'.$userProfileInfo->licenceCopy);
+                                            }
+                                            else{
+                                                $licenceLink = 'javascript:void(0);';
+                                            }
+                                            if($userProfileInfo->establishmentLicence!='')
+                                            {
+                                                $establishmentLicenceLink = base_url('uploads/user/document/'.$userProfileInfo->establishmentLicence);
+                                            }
+                                            else{
+                                                $establishmentLicenceLink = 'javascript:void(0);';
+                                            }
+                                            if($userProfileInfo->liabilityInsurance!='')
+                                            {
+                                                $liabilityInsuranceLink = base_url('uploads/user/document/'.$userProfileInfo->liabilityInsurance);
+                                            }
+                                            else{
+                                                $liabilityInsuranceLink = 'javascript:void(0);';
+                                            }
+                                            ?>
+                                        <h4>License / Certificate Copy <small><?= ($userProfileInfo->licenceCopy!='')?'(Verified)':'<a href="'.base_url('user/trust').'">Click on the link upload license</a>'; ?></small></h4>
+                                        Upload a copy of your license or certificate to avoid the hassle later.<p class="licenceCopyError"></p>
+                                    </div>
+                                    <div class="col-md-3 align-center">
+                                     <?php 
+                                      if (!empty($userProfileInfo->licenceCopy)) {
+                                        $licenceCopy  = explode(".",$userProfileInfo->licenceCopy); 
+                                            if ($licenceCopy[1] == 'pdf') { ?>
+                                             <a target="_blank" href="<?php echo $licenceLink; ?>" title="View Licence"><i style="font-size: 100px;" class="fa fa-file-pdf-o" aria-hidden="true"></i></a> 
+                                           <?php }else if ($licenceCopy[1] == 'doc' || $licenceCopy[1] == 'docx') { ?>
+                                               <a target="_blank" href="<?php echo $licenceLink; ?>" title="View Licence"><i style="font-size: 100px;" class="fa fa-file-word-o" aria-hidden="true"></i></a> 
+                                          <?php  }else{ ?>
+                                           <a target="_blank" href="<?php echo $licenceLink; ?>" title="View Licence"><img title="View Licence" src="<?php echo $licenceLink; ?>" style="height:100px;" ></a>
+                                         <?php   }
+                                     }
+                                       ?>
+                                    </div>
+                                </div>
+                                <div class="row mr15">
+                                    <div class="col-md-9">
+                                        <h4>Establishment license <small><?= ($userProfileInfo->establishmentLicence!='')?'(Verified)':'<a href="'.base_url('user/trust').'">Click on the link upload license</a>'; ?></small></h4>
+                                        If you are listing your space, verify ownership by providing us with your license<p class="establishmentLicenceError"></p>
+                                    </div>
+                                    <div class="col-md-3 align-center">
+                                        <?php 
+                                          if (!empty($userProfileInfo->establishmentLicence)) {
+                                         $establishmentLicence  = explode(".",$userProfileInfo->establishmentLicence); 
+                                            if ($establishmentLicence[1] == 'pdf') { ?>
+                                             <a target="_blank" href="<?php echo $establishmentLicenceLink; ?>" title="View Licence"><i style="font-size: 100px;" class="fa fa-file-pdf-o" aria-hidden="true"></i></a> 
+                                           <?php }else if ($establishmentLicence[1] == 'doc' || $establishmentLicence[1] == 'docx') { ?>
+                                               <a target="_blank" href="<?php echo $establishmentLicenceLink; ?>" title="View Licence"><i style="font-size: 100px;" class="fa fa-file-word-o" aria-hidden="true"></i></a> 
+                                          <?php  }else{ ?>
+                                           <a target="_blank" href="<?php echo $establishmentLicenceLink; ?>" title="View Licence"><img title="View Licence" src="<?php echo $establishmentLicenceLink; ?>" style="height:100px;" ></a>
+                                         <?php   }
+                                     }
+                                       ?>
+                                    </div>
+                                </div>
+                                <div class="row mr15">
+                                    <div class="col-md-9">
+                                        <h4>Liabilty insurance <small><?= ($userProfileInfo->liabilityInsurance!='')?'(Verified)':'<a href="'.base_url('user/trust').'">Click on the link upload insurance</a>'; ?></small></h4>
+                                        Upload a copy of your liability insurance.<p class="liabilityInsuranceError"></p>
+                                    </div>
+                                    <div class="col-md-3 align-center">
+                                       <?php 
+                                        if (!empty($userProfileInfo->liabilityInsurance)) {
+                                          $liabilityInsurance  = explode(".",$userProfileInfo->liabilityInsurance); 
+                                            if ($liabilityInsurance[1] == 'pdf') { ?>
+                                             <a target="_blank" href="<?php echo $liabilityInsuranceLink; ?>" title="View Licence"><i style="font-size: 100px;" class="fa fa-file-pdf-o" aria-hidden="true"></i></a> 
+                                           <?php }else if ($liabilityInsurance[1] == 'doc' || $liabilityInsurance[1] == 'docx') { ?>
+                                               <a target="_blank" href="<?php echo $liabilityInsuranceLink; ?>" title="View Licence"><i style="font-size: 100px;" class="fa fa-file-word-o" aria-hidden="true"></i></a> 
+                                          <?php  }else{ ?>
+                                           <a target="_blank" href="<?php echo $liabilityInsuranceLink; ?>" title="View Licence"><img title="View Licence" src="<?php echo $liabilityInsuranceLink; ?>" style="height:100px;" ></a>
+                                         <?php   }
+                                          }
+                                       ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- close here -->
                     </div>
                 </article>
             </div>
