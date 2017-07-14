21f4df063119d5b4f05867e2eb7962935ef8669d
Shashank Shekhar <shashank@neurons-it.in>
2017-07-14 17:16:17 +0530

    Manage listing

--- application/config/routes.php
+++ application/config/routes.php
@@ -71,6 +71,7 @@ $route['contactList'] = 'Dashboard/contactList';
 
 $route['listing'] = 'Listing/listing';
 $route['preview-listing/(:num)'] = 'Listing/preview_listing/$1';
+$route['manage-listing/(:num)'] = 'Listing/manage_listing/$1';
 $route['my-reservations'] = 'Listing/Listing2';
 $route['reservation-requirements'] = 'Listing/Listing3';
 
