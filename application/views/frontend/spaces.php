4f55078daf8465b091fb58ec8359d653e65a67c9
Sunil Bhaskar <sunil@neurons-it.in>
2017-07-15 18:19:13 +0530

    display of the product on the map

--- application/views/frontend/spaces.php
+++ application/views/frontend/spaces.php
@@ -68,7 +68,8 @@
 <!--                <li class=""><a herf="#">More <i class="fa fa-caret-down" aria-hidden="true"></i></a></li>-->
             </ul>
             <div class="row">
-                <?php if(!empty($listings)):?>
+                <?php 
+                   if(!empty($listings)):?>
                 <?php foreach($listings as $listing): if(isset($listing['gallery']) && !empty($listing['gallery'])){?>
                 <?php
                 $basePrice = (!empty($listing['base_price']))? getCurrency_symbol($listing['currency']). number_format($listing['base_price']):'';
@@ -126,13 +127,37 @@
             <?php endif;?>
         </div>
         <div class="col-md-4 spaces-map">
+        <!-- display all information on the map -->
+        <?php 
+        $getViewHtml = array();
+        $basePrice = $spaceTitle = $rentType= $workspaces='';
+        foreach($listings as $listing): if(isset($listing['gallery']) && !empty($listing['gallery'])){
+              $basePrice = (!empty($listing['base_price']))? getCurrency_symbol($listing['currency']). number_format($listing['base_price']):'';
+                $spaceTitle = $listing['spaceTitle'];
+                $rentType   = $listing['establishmentType'].'/'.$listing['spaceType'];
+                $workspaces = $listing['workSpaceCount']." workspaces";
+        foreach($listing['gallery'] as $image):
+            $newHTMl  =  '<span><img style="height:120px;width:100%;" src="'.base_url('uploads/user/gallery/'.$image).'"></span><div class="content mapContent"><p><strong>'.$basePrice.'.'.$spaceTitle.'</strong></p><p><span style="font-family: \'Roboto\', sans-serif;" >'.$rentType.'· </span> .'.$workspaces.'</p><div class="review">
+                <span><img src="'.base_url('theme/front/assests/img/reting-star-home.png').'" alt="" /></span>
+                <span><img src="'.base_url('theme/front/assests/img/reting-star-home.png').'" alt="" /></span>
+                <span><img src="'.base_url('theme/front/assests/img/reting-star-home.png').'" alt="" /></span>
+                <span>1 review</span>
+            </div></div>';
+        endforeach;
+         $get['gallery']   = $newHTMl;
+         $get['latitude']  = $listing['latitude'];
+         $get['longitude'] = $listing['longitude'];
+         $getViewHtml[] = $get;
+        }
+       endforeach;
+    ?>
+         <!-- close code here -->
             <div class="row">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d45980098.7345709!2d99.03158682795288!3d45.30029418059744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1496324952690" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+               <div id="googlemap" style="height:750px; width: 100%; position: relative; overflow: hidden;"></div>
             </div>
         </div>
     </div>
 </section>
-
 <script type="text/javascript">
 $(document).ready(function(){    
     $('#demo-range').daterangepicker({
@@ -295,5 +320,114 @@ $(document).ready(function(){
         });
     });
 </script>
+<style>
+/*close button has ben remove on the map*/
+.gm-style-iw + div {display: none;}
+#googleMap { width: 100%; height: 400px; top: 0; left: 0; right: 0; bottom: 0; }
+/*style the box*/  
+.gm-style .gm-style-iw {
+background-color: #fff !important;
+top: 0 !important;
+left: 0 !important;
+width: 120% !important;
+height: 100% !important;
+min-height: 150px !important;
+padding-top: 2px;
+padding-bottom: 5px;
+display: block !important;
+}    
+/*style the p tag*/
+.gm-style .gm-style-iw #google-popup p{
+/*padding: 10px;*/
+}
+.gm-style-iw + div {display: none;}
+/*style the arrow*/
+.gm-style div div div div div div div div {
+background-color: #fff !important;
+padding: 0;
+margin: 0;
+padding: 0;
+top: 0;
+color: black;
+font-size: 16px;
+}
+/*style the link*/
+.gm-style div div div div div div div div a {
+color: #f1f1f1;
+font-weight: bold;
+} 
+.content.mapContent{
+    padding-left: 17px;
+    padding-top: 5px;
+    font-family: 'Roboto', sans-serif;
+    font-size: 15px;
+} 
+</style>
+<!-- $listings -->
+<script>
+
+// console.log(getValue);
+ var markers = [];
+ var labels = '';// The array where to store the markers
+function initialize() {
+        var mapOptions = {
+            zoom: 2,
+            center: new google.maps.LatLng(31.7860603,132.0853276),
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(document.getElementById("googlemap"), mapOptions);
+  var getJson = <?php echo json_encode($getViewHtml);?>;
+  var getValue = [];
+  var get = [];
+  $.each(getJson, function(i,n) { 
+        get['0']  = n.gallery;
+        get['1']  = n.latitude;
+        get['2']  = n.longitude;
+        get['3']  = '';
+        getValue.push(get);
+        get = [];
+  });
+     console.log(getJson)
+        // passing array value
+        var locations = getValue; 
+        var marker, i;
+        var infowindow = new google.maps.InfoWindow();
+        google.maps.event.addListener(map, 'click', function() {
+            infowindow.close();
+        });
+    for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+            map: map,
+            //title: "Derby",
+            // label: {
+            // color: 'red',
+            // fontWeight: 'bold',
+            // text: 'Price',
+            // },
+            // label: "<p style='color:red;' >hello</p>",
+            animation:google.maps.Animation.DROP,
+           icon: 'http://www.codeshare.co.uk/images/blue-pin.png'
+        });
+     google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                    infowindow.setContent(locations[i][0]);
+                    infowindow.open(map, marker);
+                }
+            })(marker, i));
+            // Push the marker to the 'markers' array
+        markers.push(marker);
+        }
+        google.maps.event.addListener(map, 'click', function(event) {
+          addMarker(event.latLng, map);
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+           // The function to trigger the marker click, 'id' is the reference index to the 'markers' array.
+    function myClick(id){
+        google.maps.event.trigger(markers[id], 'click');
+    }
+            </script>
+<script async defersrc="https://maps.googleapis.com/maps/api/js?key=AIzaSyDPuLtHPY0rnHNfMTwDnnjfP_36zxpSkw4&callback=initialize"></script> 
 </body>
 </html>
